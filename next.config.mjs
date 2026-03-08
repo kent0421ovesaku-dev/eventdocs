1d1cfab990c5e614e6334e9c7c4ec64cea876029
Kent it <kyuritorishimariyaku@KentIts-MacBook.local>
2026-03-09 03:32:48 +0900

    fix: ignore ESLint during builds

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   webpack: (config) => {
     config.resolve.alias.canvas = false;
     config.resolve.alias.encoding = false;
